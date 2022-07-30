5e18f92bdfbfde42301aa5bcb74867f075b0854c
Abdelrahman <said.abdelrahman89@gmail.com>
2022-07-30 22:43:29 +0100

    Added keymap to terminate debugging session and close UI since the event listener doesn't always run

--- lua/user/dap/init.lua
+++ lua/user/dap/init.lua
@@ -2,7 +2,13 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
+function terminate_session()
+	require('dap').terminate(nil, nil)
+	require('dapui').close()
+end
+
 keymap("n", "<F5>", ":lua require('dap').continue()<CR>", opts)
+keymap("n", "<F8>", ":lua terminate_session()<CR>", opts)
 keymap("n", "<F10>", ":lua require('dap').step_over()<CR>", opts)
 keymap("n", "<F11>", ":lua require('dap').step_into()<CR>", opts)
 keymap("n", "<F12>", ":lua require('dap').step_out()<CR>", opts)
@@ -12,7 +18,7 @@ keymap("n", "<leader>ro", ":lua require('dap').repl_open()<CR>", opts)
 keymap("n", "<leader>rl", ":lua require('dap').run_last()<CR>", opts)
 
 -- Load extensions
-require 'user.dap.dapui'
+require 'user.dap.ui'
 
 -- Load the debug adapters' configurations
 require 'user.dap.cppdbg'
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system {
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	}
+	print "Installing packer close and reopen Neovim..."
+	vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -27,82 +27,82 @@ vim.cmd [[
 -- call checks if the command called completed successfully
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+	display = {
+		open_fn = function()
+			return require("packer.util").float { border = "rounded" }
+		end,
+	},
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- Essentials
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
+	-- Essentials
+	use "wbthomason/packer.nvim" -- Have packer manage itself
+	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+	use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
 
-  -- General
-  use "tpope/vim-endwise" -- A simple plugin that helps to end certain structures automatically
-  use "jiangmiao/auto-pairs" -- Insert or delete brackets, parens, quotes in pair
-  use 'andymass/vim-matchup' -- Highlight, navigate, and operate on sets of matching text. It extends vim's % key to language-specific words instead of just single characters
-  use 'tpope/vim-commentary'
-  use { 'tpope/vim-dispatch', opt = true, cmd = { 'Dispatch', 'Make', 'Focus', 'Start' } } -- Kick off builds and test suites using one of several asynchronous adapters
-  use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install",
-    setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }) -- Preview markdown on your modern browser with synchronised scrolling and flexible configuration
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-  use 'akinsho/bufferline.nvim'
-  use 'akinsho/toggleterm.nvim'
+	-- General
+	use "tpope/vim-endwise" -- A simple plugin that helps to end certain structures automatically
+	use "jiangmiao/auto-pairs" -- Insert or delete brackets, parens, quotes in pair
+	use 'andymass/vim-matchup' -- Highlight, navigate, and operate on sets of matching text. It extends vim's % key to language-specific words instead of just single characters
+	use 'tpope/vim-commentary'
+	use { 'tpope/vim-dispatch', opt = true, cmd = { 'Dispatch', 'Make', 'Focus', 'Start' } } -- Kick off builds and test suites using one of several asynchronous adapters
+	use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install",
+		setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }) -- Preview markdown on your modern browser with synchronised scrolling and flexible configuration
+	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+	use 'akinsho/bufferline.nvim'
+	use 'akinsho/toggleterm.nvim'
 
-  -- Themes
-  use 'navarasu/onedark.nvim'
-  use 'EdenEast/nightfox.nvim'
+	-- Themes
+	use 'navarasu/onedark.nvim'
+	use 'EdenEast/nightfox.nvim'
 
-  -- Install nvim-cmp related plugins
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lua'
-  -- Snippets
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'rafamadriz/friendly-snippets'
-  -- END NVIM-CMP
+	-- Install nvim-cmp related plugins
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lua'
+	-- Snippets
+	use 'L3MON4D3/LuaSnip'
+	use 'saadparwaiz1/cmp_luasnip'
+	use 'rafamadriz/friendly-snippets'
+	-- END NVIM-CMP
 
-  -- LSP
-  use 'neovim/nvim-lspconfig' -- enable LSP
-  use 'williamboman/nvim-lsp-installer' -- language server installer
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  -- END LSP
+	-- LSP
+	use 'neovim/nvim-lspconfig' -- enable LSP
+	use 'williamboman/nvim-lsp-installer' -- language server installer
+	use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+	-- END LSP
 
 	-- DAP (Debugging)
 	use 'mfussenegger/nvim-dap'
-	use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+	use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
 
-  -- Telescope
-  use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
-  use { 'nvim-telescope/telescope-media-files.nvim' } -- Media files extension
+	-- Telescope
+	use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
+	use { 'nvim-telescope/telescope-media-files.nvim' } -- Media files extension
 
-  -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ":TSUpdate" }
-  use { 'p00f/nvim-ts-rainbow' } -- Rainbow parentheses using Treesitter
-  use { 'JoosepAlviste/nvim-ts-context-commentstring' } -- Commenting plugin for files that have different languages (e.g. ts/tsx)
+	-- Treesitter
+	use { 'nvim-treesitter/nvim-treesitter', run = ":TSUpdate" }
+	use { 'p00f/nvim-ts-rainbow' } -- Rainbow parentheses using Treesitter
+	use { 'JoosepAlviste/nvim-ts-context-commentstring' } -- Commenting plugin for files that have different languages (e.g. ts/tsx)
 
-  -- Gitsigns
-  use 'lewis6991/gitsigns.nvim'
+	-- Gitsigns
+	use 'lewis6991/gitsigns.nvim'
 
-  -- Nvim-Tree
-  use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons', } }
+	-- Nvim-Tree
+	use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons', } }
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
