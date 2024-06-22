d73daf75d3973c37c098e64853e079c8400b01ba
Abdelrahman <said.abdelrahman89@gmail.com>
2024-06-22 13:39:43 +0100

    Update gitsigns

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -5,15 +5,15 @@ end
 
 gitsigns.setup {
 	signs = {
-		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "▶", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "▶", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		add = { text = "▎" },
+		change = { text = "▎" },
+		delete = { text = "▶" },
+		topdelete = { text = "▶" },
+		changedelete = { text = "▎" },
 	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-	numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-	linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+	numhl = false,    -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false,   -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
 		interval = 1000,
@@ -27,7 +27,7 @@ gitsigns.setup {
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter_opts = {
+	current_line_blame_formatter = {
 		relative_time = false,
 	},
 	sign_priority = 6,
@@ -41,8 +41,5 @@ gitsigns.setup {
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 }
