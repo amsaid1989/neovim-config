303cb1af0f1e71d1798e7dd08c7d08bf956ac222
Abdelrahman <said.abdelrahman89@gmail.com>
2024-06-22 13:40:00 +0100

    Update TreeSitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -25,13 +25,22 @@ configs.setup {
 		"tsx",
 		"rust",
 		"vim",
+		"vimdoc",
+		"lua",
 		"zig"
 	},
 	sync_install = false,
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true,        -- false will disable the whole extension
-		disable = { "" },     -- list of language that will be disabled
+		-- disable = { "" },                   -- list of language that will be disabled
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024 * 1024 -- 1 MB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 		additional_vim_regex_highlighting = false,
 	},
 	indent = { enable = true, disable = { "yaml", "python" } },
