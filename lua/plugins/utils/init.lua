f5d5ce99f137857154dcdbf69ae8d40770c15d3e
Abdelrahman <said.abdelrahman89@gmail.com>
2024-08-25 10:22:34 +0100

    Remove treesitter

--- lua/plugins/utils/init.lua
+++ lua/plugins/utils/init.lua
@@ -1,7 +1,6 @@
 return {
 	require("plugins.utils.nvim-tree"),
 	require("plugins.utils.comment"),
-	require("plugins.utils.treesitter"),
 	require("plugins.utils.telescope"),
 	require("plugins.utils.toggleterm"),
 	require("plugins.utils.cmp"),
