55085df5c85f13fc4e9f08b1e7dc5f0fae5b8df4
Abdelrahman <said.abdelrahman89@gmail.com>
2025-04-20 15:15:05 +0100

    Remove sign_define

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -2,23 +2,23 @@ local M = {}
 
 -- TODO: backfill this to template
 M.setup = function()
-	local signs = {
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "󰌵" },
-		{ name = "DiagnosticSignInfo", text = "" },
-	}
-
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	end
-
 	local config = {
 		-- disable virtual text
 		virtual_text = true,
 		-- show signs
 		signs = {
-			active = signs,
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN]  = "",
+				[vim.diagnostic.severity.HINT]  = "󰌵",
+				[vim.diagnostic.severity.INFO]  = "",
+			},
+			linehl = {
+				[vim.diagnostic.severity.ERROR] = "Error",
+				[vim.diagnostic.severity.WARN]  = "Warning",
+				[vim.diagnostic.severity.HINT]  = "Hint",
+				[vim.diagnostic.severity.INFO]  = "Info",
+			}
 		},
 		update_in_insert = true,
 		underline = true,
